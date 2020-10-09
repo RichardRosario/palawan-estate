c9dcd4774516a15922fc04b825071546f19b00ec
Richard Rosario <tech2js@gmail.com>
2020-10-09 13:28:36 +0800

    added accounts.urls

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -16,7 +16,13 @@ class UserAccountManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, password):
-        pass
+        user = self.create_superuser(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
@@ -26,3 +32,15 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
--- backend/palEstate/settings.py
+++ backend/palEstate/settings.py
@@ -138,10 +138,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSIOM_CLASSES': ['rest_framework.permission.IsAuthenticated'],
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
-    'DEFAULT_PAGINATION_CLASSES': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 FILE_UPLOAD_PERMISSIONS = 0o640
+
+AUTH_USER_MODEL = 'accounts.UserAccount'
