e6c39b647a4aa1f8ec18112df7795d1315667c9e
Richard Rosario <tech2js@gmail.com>
2020-10-21 12:55:03 +0800

    working the frontend login and signup display

---
+++ frontend/src/containers/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <div className="signup-container">
+            This is the signup page
+        </div>
+    )
+}
+
+export default SignUp
